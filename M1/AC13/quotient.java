274a675885d7def90e246276a1781ec908705452
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-28 00:10:47 +0530

    remainder

---
+++ M1/AC13/quotient.java
@@ -0,0 +1,10 @@
+class quotientRemainder {
+	public static void main(final String[] args){
+		int a = 10;
+		int b = 5;
+		int c = a/b;
+		int d = a%b;
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
