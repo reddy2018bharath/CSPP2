684ad9da2ec43d7d4cea1582090544b7734b5a1f
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-28 01:03:44 +0530

    assign1

---
+++ M1/Assignment-1/Solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+	public static void main(String[] args) {
+		String name= "Bharath";
+		int rollnumber= 20186001;
+		System.out.println("Name :" + name + " " + " Roll Number: " + rollnumber);
+	}
+}
