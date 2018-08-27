50ea7a647fa3e218ee3955f33d862e82d40b11d5
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-28 01:12:11 +0530

    assign3

---
+++ M1/Assignment-3/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double degreesCelsius = scan.nextDouble();
+		double c = (degreesCelsius*9)/5 + 32;
+		//calculateFromDegreesToFarenheit(degreesCelsius);
+		System.out.println(c);
+	}
+	/*
+	Need to fill the calculateFromDegreesToFarenheit function and print the output
+	of fahrenheit.
+	*/
+}
