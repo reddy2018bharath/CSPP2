bfdc83fb454c1cef888b4625c88156b3067332cb
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-28 20:42:51 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp assignments/M2/Assignment-3/Solution.java
+++ cspp assignments/M2/Assignment-3/Solution.java
@@ -1,20 +1,35 @@
 import java.util.Scanner;
+/*This is comment.*/
 
-public class Solution {
-	/*
-	Do not modify this main function.
+final class Solution {
+	/**
+	*Do not modify this main function.
 	*/
-	public static void main(String[] args) {
+	private Solution() { }
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner s=new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
-        long result=power(base,exponent);
+        long result = power(base,exponent);
         System.out.println(result);
 	}
 	/*
 	Need to write the power function and print the output.
 	*/
-	public static long power(int base,int exponent)
+    /**
+     * { function_description }
+     *
+     * @param      base      The base
+     * @param      exponent  The exponent
+     *
+     * @return     { description_of_the_return_value }
+     */
+	public static long power(final int base,final int exponent)
 	{
 		if (exponent==0)
 		{
