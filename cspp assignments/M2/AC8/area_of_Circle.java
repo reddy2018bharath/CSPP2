3273a48901de61cbd31ad8ada3047aac47fc70df
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-28 22:09:42 +0530

    area

---
+++ cspp assignments/M2/AC8/area_of_Circle.java
@@ -0,0 +1,16 @@
+public class area_of_Circle {
+    public static void main(String[] args) 
+    {
+    	area(7); 
+    	
+    }
+    static double pi()
+    {
+    	return 3.14;
+    }
+    static void area(int r)
+    {
+    	System.out.println(r*r*pi());
+    	
+    }
+}
