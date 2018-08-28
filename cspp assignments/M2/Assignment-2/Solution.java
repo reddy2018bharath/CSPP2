00e7463d7e03bcd5c47e2b63a5e34184cfa4d45f
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-28 20:15:32 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp assignments/M2/Assignment-2/Solution.java
+++ cspp assignments/M2/Assignment-2/Solution.java
@@ -1,37 +1,33 @@
 import java.util.Scanner;
-import java.lang.Math;
-
 public class Solution {
-	/**
-	*Do not modify this main function.
-	*/
-	protected Solution() { }
-	/**
-	*Need to write the rootsOfQuadraticEquation function and print the output.
-	*@param args is an argument
-	 */
-	public static void main(final String[]  args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/**
-	*Need to write the rootsOfQuadraticEquation.
-	*@param a is an argument
-	*@param b is an argument
-	*@param c is an argument
-	*/
+    /**
+    *Do not modify this main function.
+    */
+    protected Solution() { }
+    /**
+    *Need to write the rootsOfQuadraticEquation function and print the output.
+    *@param args is an argument
+     */
+    public static void main(final String[]  args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /**
+    *Need to write the rootsOfQuadraticEquation.
+    *@param a is an argument
+    *@param b is an argument
+    *@param c is an argument
+    */
 
-	public static void rootsOfQuadraticEquation(final int a,
-	 final int b, final int c) {
-		final int x = 4;
-		double r1 = (-b + Math.sqrt((b * b) - (x * a * c))) / (2 * a);
-		double r2 = (-b - Math.sqrt((b * b) - (x * a * c))) / (2 * a);
-		System.out.println(r1 + " " + r2);
-
-
-	}
+    public static void rootsOfQuadraticEquation(final int a,
+     final int b, final int c) {
+        final int x = 4;
+        double r1 = (-b + Math.sqrt((b * b) - (x * a * c))) / (2 * a);
+        double r2 = (-b - Math.sqrt((b * b) - (x * a * c))) / (2 * a);
+        System.out.println(r1 + " " + r2);
+    }
 }
    
