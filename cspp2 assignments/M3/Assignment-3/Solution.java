763c9030629225257c1659efb30f92e6292d359a
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-29 18:53:40 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 assignments/M3/Assignment-3/Solution.java
+++ cspp2 assignments/M3/Assignment-3/Solution.java
@@ -6,14 +6,19 @@ import java.util.Scanner;
  * Class for solution.
  */
 
-public class Solution {
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-    public static void main(String[] args) {
+    }
+    /**
+     * { function_description }.
+     * @param      args  The arguments
+     */
+
+    public static void main(final String[] args) {
 
         Scanner s = new Scanner(System.in);      
         int n1 = s.nextInt();
@@ -28,16 +33,18 @@ public class Solution {
      *
      * @return     { description_of_the_return_value }
      */
-	public static int gcd(int n1,int n2) {
-		int r = 1;
-		while (r > 0) {
-			r = n1 % n2;
-			if (r == 0) {
-				return n2;
-			}
-			n1 = n2;
-			n2 = r;	
-		}
-		return 1;
-	}
+    public static int gcd(final int m1,final int m2) {
+        int n1 = m1;
+        int n2 = m2;
+        int r = 1;
+        while (r > 0) {
+            r = n1 % n2;
+            if (r == 0) {
+                return n2;
+            }
+            n1 = n2;
+            n2 = r; 
+        }
+        return 1;
+    }
 }
