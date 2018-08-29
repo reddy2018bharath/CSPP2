3a122eaed78a4684a1e783ff1f4c496fbcc00643
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-29 19:05:28 +0530

    pattern

---
+++ cspp2 practice/MODULE 3/ACTIVITY8/Pattern.java
@@ -0,0 +1,12 @@
+public class Pattern {
+	public static void main(String[] args) {
+		int i = 5;
+		for (int a =1; a<=5; ++a){
+			for (int b=1; b<=a; ++b) {
+				System.out.print(b+" ");
+			}
+			System.out.println();
+		}
+		
+		}
+	}
