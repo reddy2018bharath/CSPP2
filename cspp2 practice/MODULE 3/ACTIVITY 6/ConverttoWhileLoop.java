ac2d97e489e4dd8241e5b8dee20e55ee53268a9f
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-29 19:00:52 +0530

    convert

---
+++ cspp2 practice/MODULE 3/ACTIVITY 6/ConverttoWhileLoop.java
@@ -0,0 +1,10 @@
+public class ConverttoWhileLoop {
+    public static void main(String[] args) {
+    	int i = 0;
+    	while (i<10) {
+    		i = i+2;
+    		System.out.println("System.out.println"+"("+i+")");
+    	}
+    	System.out.println("System.out.println(GoodBye!)");
+    }
+}
