5324757d9efdbb69d460957426273432439c4577
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-29 17:24:24 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 assignments/M3/Assignment-2/Solution.java
+++ cspp2 assignments/M3/Assignment-2/Solution.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
+/**
+ * Constructs the object.
+ */
     private Solution() {
 
     }
