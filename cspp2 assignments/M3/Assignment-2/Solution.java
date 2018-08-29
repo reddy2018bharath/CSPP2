277759b25b2bc0e6e2575f9e7c543f7e19c920ac
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-29 17:23:27 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 assignments/M3/Assignment-2/Solution.java
+++ cspp2 assignments/M3/Assignment-2/Solution.java
@@ -1,28 +1,34 @@
+/**
+ * { item_description }.
+ */
 import java.util.Scanner;
 /**
  * Class for solution.
  */
 public class Solution {
+    private Solution() {
+
+    }
 /**
- * { function_description }
+ * { function_description }.
  *
  * @param      args  The arguments
  */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         final int d = 10;
         final int f = 7;
         int c = 0, k;
         for (int i = 1; i <= n; i++) {
-        	int j = i;
-        	while (j > 0) {
-        	k = j % d;
-        	if (k == f ) {
-        		c = c + 1;
-        	}
-        	j = j / d;
+            int j = i;
+            while (j > 0) {
+            k = j % d;
+            if (k == f) {
+                c = c + 1;
+            }
+            j = j / d;
         }
        }
      System.out.println(c);
