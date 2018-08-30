893d5bc432f223029536f7b84c3fc28e3a2be90b
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-30 14:10:18 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2 assignments/M4/Assignment-1/Solution.java
+++ cspp2 assignments/M4/Assignment-1/Solution.java
@@ -1,8 +1,11 @@
 /**
  * { item_description }.
  */
-import java.util.*;
-public class Solution {
+import java.util.Scanner;
+/**
+ * { item_description }.
+ */
+public final class Solution {
 /**
  * { function_description }.
  */
@@ -13,22 +16,17 @@ private Solution () {
  * @param      args  The arguments
  */
 
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int k = 0;
-		int a[] = new int[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = sc.nextInt();
-			if (a[i] > k) {
-				k = a[i];
-			}
-		
-		}
-		System.out.println(k);
-
-		
-
-	}
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = 0;
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            if (a[i] > k) {
+                k = a[i];
+            }
+        }
+        System.out.println(k);
+    }
 }
