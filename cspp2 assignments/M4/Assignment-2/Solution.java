cb699dcf205209bdcdf4a5543e19a55e263f7cde
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-30 15:13:29 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2 assignments/M4/Assignment-2/Solution.java
+++ cspp2 assignments/M4/Assignment-2/Solution.java
@@ -17,47 +17,46 @@ private Solution() {
  *
  * @param      args  The arguments
  */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc .nextInt();
-		int[][] c = new int[a][b];
-		for (int i = 0; i < a; i++) {
-			for (int j = 0; j < b; j++) {
-				c[i][j] = sc.nextInt();
-			}
-		}
-		int d = sc.nextInt();
-		int e = sc .nextInt();
-		int[][] f = new int[d][e];
-		for (int i = 0; i < d; i++) {
-			for (int j = 0; j < e; j++) {
-				f[i][j] = sc.nextInt();
-			}
-		}
-		if (a != d && b != e) {
-			System.out.println("not possible");
-		}
-		else {
-			int[][] g = new int[a][b];
-			int i = 0;
-			int j = 0;
-			for (i = 0; i < a; i++) {
-				for (j = 0; j < b; j++) {
-					g[i][j] = c[i][j] + f[i][j];
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc .nextInt();
+        int[][] c = new int[a][b];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                c[i][j] = sc.nextInt();
+            }
+        }
+        int d = sc.nextInt();
+        int e = sc .nextInt();
+        int[][] f = new int[d][e];
+        for (int i = 0; i < d; i++) {
+            for (int j = 0; j < e; j++) {
+                f[i][j] = sc.nextInt();
+            }
+        }
+        if (a != d && b != e) {
+            System.out.println("not possible"); }
+        else {
+            int[][] g = new int[a][b];
+            int i = 0;
+            int j = 0;
+            for (i = 0; i < a; i++) {
+                for (j = 0; j < b; j++) {
+                    g[i][j] = c[i][j] + f[i][j];
 
-			}
-		}
-		for (i = 0; i < a; i++) {
-				for (j = 0; j < b; j++) {
-					System.out.print(g[i][j]);
-					if (j != b - 1) {
-						System.out.print(" ");
-					}
-			}
-			System.out.println();
-		}
-	}
+            }
+        }
+        for (i = 0; i < a; i++) {
+                for (j = 0; j < b; j++) {
+                    System.out.print(g[i][j]);
+                    if (j != b - 1) {
+                        System.out.print(" ");
+                    }
+            }
+            System.out.println();
+        }
+    }
    }
  }
 
