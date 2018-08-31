dee1423d9dba3f4bd0148f8afa4445a3094620a7
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-31 19:09:09 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 assignments/M4/Assignment-4/Solution.java
+++ cspp2 assignments/M4/Assignment-4/Solution.java
@@ -5,41 +5,36 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution
-{
+public final class Solution {
 /**
  * Constructs the object.
  */
-	private Solution() {
+    private Solution() {
 
-	}
+    }
 /**
  * { function_description }.
  *
  * @param      args  The arguments.
  */
-	public static void main(final String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);
-		
-	}
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);  
+        System.out.println(reverse);
+        }
 /**
- * { function_description }
+ * { function_description }.
  *
- * @param      s     { parameter_description }
+ * @param      s     { parameter_description }.
  *
- * @return     { description_of_the_return_value }
+ * @return     { description_of_the_return_value }.
  */
-	public static String reverseString(final String s) {
-		String k = "";
-		for (int i = s.length()-1; i >= 0; i--) {
-			k += s.charAt(i);
-		}
-		return k;
-	}
-	
-
+    public static String reverseString(final String s) {
+        String k = "";
+        for (int i = s.length()-1; i >= 0; i--) {
+            k += s.charAt(i);
+        }
+        return k;
+    }
 }
