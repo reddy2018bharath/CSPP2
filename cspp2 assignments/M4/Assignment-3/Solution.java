f1dffd44e51b6db68edc4c8c4a8081a60d730e20
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-31 17:54:02 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- cspp2 assignments/M4/Assignment-3/Solution.java
+++ cspp2 assignments/M4/Assignment-3/Solution.java
@@ -5,47 +5,43 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution
-{/*
-	Do not modify this main function.
-	*/
-	private Solution() {
+public final class Solution {/*
+    Do not modify this main function.
+    */
+    private Solution() {
 
-	}
+    }
 /**
- * { function_description }
+ * { function_description }.
  *
  * @param      args  The arguments.
  */
-	public static void main(final String[] args) {
+    public static void main(final String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.nextLine();
-		for(int i = 0; i < n; i++){
-			String s = sc.next();
-			String res = binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-		static String binaryToDecimal(String a) {
-			Long d = Long.parseLong(a);
-			double r = 0;
-			double digit = 0;
-			int c = a.length();
-			for (int i = 0; i < c; i++) {
-				if (a.charAt(i) == '1' || a.charAt(i) == '0') {
-					double f = 0;
-			digit = d % 10;
-			d = d / 10;
-			f = digit * (Math.pow(2,i));
-			r = r + f;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for(int i = 0; i < n; i++) {
+            String s = sc.next();
+            String res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+        static String binaryToDecimal(String a) {
+            //Long d = Long.parseLong(a);
+            int total = 0, value =0;
+            int c = a.length();
+            for (int i = 0; i < c; i++) {
+                if (a.charAt(i) == '1' || a.charAt(i) == '0') {
+              value = Character.getNumericValue(a.charAt(i));
+              total += value*Math.pow(2, c-1);
+              c--;
 
-				}
-			}
-			
-			int g = (int)r;
-			return String.valueOf(g);
-		}
-	}
+                }
+            }
+            
+
+            return String.valueOf(total);
+        }
+    }
 
