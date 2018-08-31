0836ba6cd67172f758af44808f919165a067259d
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-08-31 16:15:52 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- cspp2 assignments/M4/Assignment-3/Solution.java
+++ cspp2 assignments/M4/Assignment-3/Solution.java
@@ -1,7 +1,5 @@
 
 // Given an String, Write a java method that returns the decimal value for the given binary string.
-
-
 import java.util.Scanner;
 public class Solution
 {/*
@@ -18,7 +16,7 @@ public class Solution
 			System.out.println(res);
 		}
 	}
-		public static String binaryToDecimal(String a) {
+		static String binaryToDecimal(String a) {
 			Long d = Long.parseLong(a);
 			double r = 0;
 			double digit = 0;
