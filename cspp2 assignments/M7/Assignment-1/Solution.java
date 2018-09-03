ae0e80279e18eb4c574e697aad1af491bbc61d6e
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-09-03 20:40:20 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2 assignments/M7/Assignment-1/Solution.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+/**This is a program.**/
+final class InputValidator {
+    /**
+     * @param input comment
+     */
+    private int l;
+    /**
+     * @param input The input
+     */
+    InputValidator(final String input) {
+        l = input.length();
+    }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }.
+     */
+    boolean validateData() {
+        final int six = 6;
+        if (l >= six) {
+            return true;
+        }
+        return false;
+    }
+}
+/*Program.*/
+/**
+ * { item_description }.
+ */
+final class Solution {
+    /**
+     * '{ item_description }.
+     */
+    private Solution() { }
+    /**
+     * @param args vaLue
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }.
+         */
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
+    }
+}
