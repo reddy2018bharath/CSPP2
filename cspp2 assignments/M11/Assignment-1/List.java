5f2b89bd57428a20fed9f1003328ea7a74f1d317
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-09-08 16:13:31 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2 assignments/M11/Assignment-1/List.java
+++ cspp2 assignments/M11/Assignment-1/List.java
@@ -417,7 +417,7 @@ public class List {
                         l.removeAll(a);
                     }
                 break;
-                case "subList": {
+                case "subList":
                     if (tokens.length != 2) {
                         break;
                     }
@@ -428,7 +428,6 @@ public class List {
                         System.out.println(object);
                     }
                     break;
-                }
                 case "equals":
                     if (tokens.length == 2) {
                         String[] lt = tokens[1].split(",");
