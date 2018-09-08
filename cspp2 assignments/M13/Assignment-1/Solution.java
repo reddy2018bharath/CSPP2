f5ffb2ca15c99aa165128cb3b7888dfdc8b13132
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-09-08 17:56:42 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2 assignments/M13/Assignment-1/Solution.java
+++ cspp2 assignments/M13/Assignment-1/Solution.java
@@ -37,7 +37,7 @@ class Set {
         String str = "{";
         int i = 0;
         for (i = 0; i < size - 1; i++) {
-            str = str + list[i] + ",";
+            str = str + list[i] + ", ";
         }
         str = str + list[i] + "}";
         return str;
