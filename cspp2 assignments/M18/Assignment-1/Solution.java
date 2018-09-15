05623ed3a65a8147584b49f5d27ee85ab0631210
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-09-15 12:27:29 +0530

    "check_style_score -> 1 of 13 passed. style score: 0/1 "

--- cspp2 assignments/M18/Assignment-1/Solution.java
+++ cspp2 assignments/M18/Assignment-1/Solution.java
@@ -82,6 +82,7 @@ public final class Solution {
     		 	System.out.println("Invalid penalty for question about sony");
 
     		 }
+    		 System.out.println(tokens.length);
     		 if (tokens.length != 5) {
     		 	System.out.println("Error! Malformed question");
     		 }
