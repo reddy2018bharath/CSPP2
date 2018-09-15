7f1c59add55b33be03d2be75ec5fc5d6ee0a0357
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-09-15 12:49:26 +0530

    "check_style_score -> 4 of 13 passed. style score: 0/1 "

--- cspp2 assignments/M18/Assignment-1/Solution.java
+++ cspp2 assignments/M18/Assignment-1/Solution.java
@@ -75,6 +75,11 @@ public final class Solution {
     		 String line = s.nextLine();
     		 String[] tokens = line.split(":");
     		 //System.out.println(tokens);
+    		 for (int i =0; i < 5; i++) {
+    		 	if (tokens[i].length() == 0) {
+    		 		System.out.println("Error! Malformed question");
+    		 	}
+    		 }
     		 if (Integer.parseInt(tokens[3]) <= 0) {
     		 	System.out.println("Invalid max marks for question about sony");
     		 }
@@ -82,11 +87,7 @@ public final class Solution {
     		 	System.out.println("Invalid penalty for question about sony");
 
     		 }
-    		 for (int i =0; i < 5; i++) {
-    		 	if (tokens[i].length() == 0) {
-    		 		System.out.println("Error! Malformed question");
-    		 	}
-    		 }
+    		 
 
     		 //System.out.println(tokens[0].length());
     		 if (tokens[4].length() == 0) {
