2bebb2db853d425691ebb80e54b8e0b0ea3bfeed
reddy2018bharath <bharathkumarreddy55@msitprogram.net>
2018-09-15 13:18:43 +0530

    "check_style_score -> 2 of 13 passed. style score: 0/1 "

--- cspp2 assignments/M18/Assignment-1/Solution.java
+++ cspp2 assignments/M18/Assignment-1/Solution.java
@@ -97,10 +97,10 @@ public final class Solution {
     		 if (Integer.parseInt(tokens[d]) > 0) {
     		 	System.out.println("Invalid penalty for"+ " " + tokens[0]);
     		 }
-    		 /*String[] array = tokens[2].split(" ");
-    		 if(array.length() < 2) {
-    		 	System.out.println();
-    		 }*/
+    		 String[] array = tokens[2].split(" ");
+    		 if(array.length < 2) {
+    		 	System.out.println(tokens[0] + " " + "does not have enough answer choices");
+    		 }
     	}
     }
         
